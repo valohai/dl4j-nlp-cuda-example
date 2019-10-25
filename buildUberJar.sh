660eb2378568b957036e9a3871bd2b572e01a2aa
mani <sadhak001@gmail.com>
2019-10-25 01:21:51 +0100

    Inlining the build uber jar commands from valohai.yaml into buildUberJar.sh

--- buildUberJar.sh
+++ buildUberJar.sh
@@ -35,9 +35,18 @@ if [[ "${OSNAME}" = "macosx" ]]; then
     echo "Skymind has dropped support for CUDA for the MacOSX, " \
          "see https://gitter.im/deeplearning4j/deeplearning4j?at=5d92a15c9d4cf173604945c6."
 else
-    VH_OUTPUTS_DIR=${VH_OUTPUTS_DIR:-"."}
-    ./know-your-gpus.sh &> "${VH_OUTPUTS_DIR}/build-time-know-your-gpus.logs"
-
     echo ""; echo "Building uber jar for target platform: ${OSNAME} with GPU backend"
     runMvnPackage "gpu-backend"
+fi
+
+### Check if we are running in the Valohai environment, an empty VH_INPUTS_DIR means we are running outside that environment
+if [[ ! -z "${VH_OUTPUTS_DIR:-}"]]; then
+    ### In Valohai environment
+    echo "~~~ Copying the build-time-know-your-gpus.logs file into ${VH_OUTPUTS_DIR}"
+    ./know-your-gpus.sh &> "${VH_OUTPUTS_DIR}/build-time-know-your-gpus.logs"
+
+    echo "~~~ Copying the build jar file into ${VH_OUTPUTS_DIR}"
+    cp target/*bin*.jar ${VH_OUTPUTS_DIR}
+
+    ls -lash ${VH_OUTPUTS_DIR}
 fi
