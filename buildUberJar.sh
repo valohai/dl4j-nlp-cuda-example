eb1e083f0594c3645363bf9086fff6fd39bb4bc5
mani <sadhak001@gmail.com>
2019-10-25 01:32:39 +0100

    Refactored code in buildUberJar.sh

--- buildUberJar.sh
+++ buildUberJar.sh
@@ -16,9 +16,9 @@ detectOSPlatform() {
 	echo ${OSNAME}
 }
 
-./download-src-main-resources.sh
-
-OSNAME=${1:-$(detectOSPlatform)}
+downloadResources() {
+    ./download-src-main-resources.sh
+}
 
 runMvnPackage() {
     BACKEND=${1:-"cpu-backend"}
@@ -27,26 +27,38 @@ runMvnPackage() {
                  -Dplatform="${OSNAME}"
 }
 
-echo ""; echo "Building uber jar for target platform: ${OSNAME} with CPU backend (default)"
-runMvnPackage "cpu-backend"
+runBuildCpuBackendPackage() {
+    echo ""; echo "Building uber jar for target platform: ${OSNAME} with CPU backend (default)"
+    runMvnPackage "cpu-backend"
+}
 
-if [[ "${OSNAME}" = "macosx" ]]; then
-    echo ""; echo "Skipping building of uber jar for target platform: ${OSNAME} with GPU backend..."
-    echo "Skymind has dropped support for CUDA for the MacOSX, " \
-         "see https://gitter.im/deeplearning4j/deeplearning4j?at=5d92a15c9d4cf173604945c6."
-else
-    echo ""; echo "Building uber jar for target platform: ${OSNAME} with GPU backend"
-    runMvnPackage "gpu-backend"
-fi
+runBuildGpuBackendPackage() {
+    if [[ "${OSNAME}" = "macosx" ]]; then
+        echo ""; echo "Skipping building of uber jar for target platform: ${OSNAME} with GPU backend..."
+        echo "Skymind has dropped support for CUDA for the MacOSX, " \
+             "see https://gitter.im/deeplearning4j/deeplearning4j?at=5d92a15c9d4cf173604945c6."
+    else
+        echo ""; echo "Building uber jar for target platform: ${OSNAME} with GPU backend"
+        runMvnPackage "gpu-backend"
+    fi
+}
 
-### Check if we are running in the Valohai environment, an empty VH_INPUTS_DIR means we are running outside that environment
-if [[ ! -z "${VH_OUTPUTS_DIR:-}"]]; then
-    ### In Valohai environment
-    echo "~~~ Copying the build-time-know-your-gpus.logs file into ${VH_OUTPUTS_DIR}"
-    ./know-your-gpus.sh &> "${VH_OUTPUTS_DIR}/build-time-know-your-gpus.logs"
+postBuildProcessValohaiPlatform() {
+    ### Check if we are running in the Valohai environment, an empty VH_INPUTS_DIR means we are running outside that environment
+    if [[ ! -z "${VH_OUTPUTS_DIR:-}"]]; then
+        ### In Valohai environment
+        echo "~~~ Copying the build-time-know-your-gpus.logs file into ${VH_OUTPUTS_DIR}"
+        ./know-your-gpus.sh &> "${VH_OUTPUTS_DIR}/build-time-know-your-gpus.logs"
 
-    echo "~~~ Copying the build jar file into ${VH_OUTPUTS_DIR}"
-    cp target/*bin*.jar ${VH_OUTPUTS_DIR}
+        echo "~~~ Copying the build jar file into ${VH_OUTPUTS_DIR}"
+        cp target/*bin*.jar ${VH_OUTPUTS_DIR}
 
-    ls -lash ${VH_OUTPUTS_DIR}
-fi
+        ls -lash ${VH_OUTPUTS_DIR}
+    fi
+}
+
+OSNAME=${1:-$(detectOSPlatform)}
+downloadResources
+runBuildCpuBackendPackage
+runBuildGpuBackendPackage
+postBuildProcessValohaiPlatform
